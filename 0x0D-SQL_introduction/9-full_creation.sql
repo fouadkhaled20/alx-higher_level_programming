8e8341ec5e8770d8f2d9a0c5f685e79e17c862a2
fouad khaled <fouad@khaled.com>
2024-03-26 08:34:27 -0700

    a

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- usage: cat 4-first_table.sql | mysql -hlocalhost -uroot -p <database name>
+-- insert multiple values in a database
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT
+  INTO second_table
+		(id, name, score)
+VALUES	(1, 'John', 10),
+		(2, 'Alex', 3),
+		(3, 'Bob', 14),
+		(4, 'George', 8);

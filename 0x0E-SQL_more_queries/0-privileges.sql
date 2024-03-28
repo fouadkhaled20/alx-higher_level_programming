ee5231b433f2c212796fc4499d1a08a2c055dda5
fouad khaled <fouad@khaled.com>
2024-03-28 05:17:06 -0700

    a

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- show privliges of some users in a server
+
+-- show privliges of user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+
+-- show privliges of user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
